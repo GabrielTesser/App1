49d27aa7326da75d9f6d7c9eeb6b7fc95f08f0f3
Usuario <Usuario@CPD03-M19>
2023-05-15 15:17:14 -0300

    Adicionar arquivos de projeto.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using App1.Model;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Selecione uma imagem"
+            });
+
+            if (result != null)
+            {
+                cavalos.Source = ImageSource.FromFile(result.FullPath);
+
+                Compartilhado.SelectedImage = ImageSource.FromFile(result.FullPath);
+
+
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
---
+++ App1/App1/Model/Compartilhado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1.Model
+{
+    public class Compartilhado
+    {
+        public static ImageSource SelectedImage { get; set; }
+
+    }
+}
